6f35f1c77cce215c39d5a3a4edfec301869dd59c
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2015-12-04 03:48:48 -0800

    3 December 2015: a third of the way through #002. Stopping before Aeolus.

--- chapter-scripts/1.py
+++ chapter-scripts/1.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
-"""Script to create the text generated based on the first chapter of Joyce's Ulysses, 'Telemachus.'
-Currently, just returns the actual text of 'Telemachus' exactly as Joyce wrote it.
+"""Script to create the text generated based on the first chapter of Joyce's
+Ulysses, 'Telemachus.' Currently, just generates 366 sentences based on the
+text of 'Telemachus, aiming for an average paragraph length of 3.7, which
+is the actual number of sentences and average paragraph length from that chapter
+
+A Markov length of 3 seems to work well here, according to insufficient tests
+evaluated informally.
 """
 
+# First, set up constants
+markov_generator_path = '/UlyssesRedux/code/markov-sentence-generator'
+joyce_base_text_path = '/UlyssesRedux/corpora/joyce/ulysses/01.txt'
+chain_length = 3
+chapter_length = 366                # Measured in sentences.
+sentences_per_paragraph = 3.7        # On average, in this chapter
+
 import sys
-sys.path.append('/UlyssesRedux/code/markov-sentence-generator')
+sys.path.append(markov_generator_path)
 from sentence_generator import *
 
-starts, the_mapping = buildMapping(word_list('/UlyssesRedux/corpora/joyce/ulysses/01.txt'), markov_length=2)
-print(gen_text(the_mapping, starts, markov_length=2, sentences_desired=24, paragraph_break_probability=0.2))
+starts, the_mapping = buildMapping(word_list(joyce_base_text_path), markov_length=chain_length)
+print(gen_text(the_mapping, starts, markov_length=chain_length, sentences_desired=chapter_length, paragraph_break_probability=(1/sentences_per_paragraph)))
--- utility-scrips/get-chapter-stats.py
+++ utility-scrips/get-chapter-stats.py
@@ -3,17 +3,22 @@
 in a chapter. Makes a lot of assumptions about the structure of the text file,
 including one paragraph per line with no blank lines.
 
-use: get_chapter_stats FILE, where FILE is the chapter filename.
+usage: get_chapter_stats FILE, where FILE is the chapter filename.
 """
+import sys, os
 
 filename = sys.argv[1]
 
-the_text = open('/UlyssesRedux/corpora/joyce/ulysses/%s' % filename).read()
+the_text = open(filename).read()
+
 indiv_sents = the_text.split('\n')
 paragraph_stats_list = []
 for which_sent in range(len(indiv_sents)):
     paragraph_stats_list.append([1 + which_sent, len(indiv_sents[which_sent].split("."))])
-the_stats_file = open('%s-stats.txt' % filename, 'w')
+
+stats_file_name = os.path.splitext(filename)[0] + '-stats.csv'
+
+the_stats_file = open('%s-stats.csv' % filename, 'w')
 the_stats_file.write('Paragraph #,# of sentences\n')
 for the_para in paragraph_stats_list:
   the_stats_file.write("%d, %d\n" % (the_para[0], the_para[1]) )
