e0bab91e97fb1a8e86d471bb196f2cde90214489
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-04-17 17:00:37 -0700

    17 Apr 2017: fixing tiny stupid error

--- utility_scripts/setup-run.py
+++ utility_scripts/setup-run.py
@@ -9,7 +9,7 @@ import os, glob, csv, subprocess
 
 import sys
 sys.path.append('/UlyssesRedux/scripts/')
-from directory_structure import *           # Gets us the listing of file and directory locations. 
+from directory_structure import *           # Gets us the listing of file and directory locations.
 
 # First, remove the old index file
 if os.path.isfile(toc_fragment):
@@ -29,7 +29,7 @@ while not is_done:
     print("\n\nOK, let's set up the parameters for the next run.")
     print("You can type (all-caps) SAME at any prompt to re-use the last run's answer to that question.\n")
     current_run_data = {}.copy()
-    for which_key in sorted(last_run_data.items()):
+    for which_key in sorted(last_run_data.keys()):
         answer = input('%s (previously "%s") ---|  ' % (which_key, last_run_data[which_key]))
         if answer == "SAME":
             current_run_data[which_key] = last_run_data[which_key]
